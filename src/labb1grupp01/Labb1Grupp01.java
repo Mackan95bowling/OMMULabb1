7ce0fefb513d04d46186f8a6314c57ebff02c9d2
s143534 <s143534@PC966809.ida.utb.hb.se>
2016-09-06 08:16:18 +0200

    new

--- src/labb1grupp01/Labb1Grupp01.java
+++ src/labb1grupp01/Labb1Grupp01.java
@@ -17,7 +17,7 @@ public class Labb1Grupp01 {
     public static void main(String[] args) {
         // TODO code application logic here
         //tjenare hallå
-        
+        //hejigen
     }
     
 }
